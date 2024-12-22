7a04189411ac325ff1b87df44e701399bc022ed6
zchmielewska <zchmielewska@gmail.com>
2024-12-22 10:37:42 +0100

    fix runplan

--- input.py
+++ input.py
@@ -13,6 +13,6 @@ policy = ModelPointSet(data=pd.DataFrame({
 
 
 runplan = Runplan(data=pd.DataFrame({
-    "version": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "version": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     "interest_rate_stress": [0, 0.1, -0.1, 0.2, -0.2, 0.3, -0.3, 0.4, -0.4, 0.5, -0.5]
 }))
