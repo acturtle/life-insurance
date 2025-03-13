c60c92dc16005ee912f49f4f43e2375307c64df5
zchmielewska <zchmielewska@gmail.com>
2025-03-13 19:29:42 +0100

    output to S3

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
+import boto3
 import os
-from cashflower import run
+from cashflower import run, parse_arguments
+from io import StringIO
 from settings import settings
 
 
 if __name__ == "__main__":
-    output = run(settings=settings, path=os.path.dirname(__file__))
+    output, diagnostic, log = run(settings=settings, path=os.path.dirname(__file__))
+    args, unknown = parse_arguments()
+    filename = unknown[1]
+
+    # Upload to S3 bucket
+    s3_client = boto3.client('s3')
+    csv_buffer = StringIO()
+    output.to_csv(csv_buffer, index=False)
+    s3_client.put_object(
+        Bucket="actuhub-dev",
+        Key=f"{filename}",
+        Body=csv_buffer.getvalue()
+    )
